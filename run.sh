b465a65e2ea23e6fde0854566b7bf15f724a6c27
raacker <haven.cpp@gmail.com>
2017-04-06 18:45:14 +0900

    upload

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt-get install python3
+apt-get install python3-pip
+pip install --upgrade pip
+if [ -f /usr/bin/pip ]; then
+	rm -rf /usr/bin/pip
+fi
+sudo ln -s /usr/local/bin/pip /usr/bin/pip
+
+pip install numpy
