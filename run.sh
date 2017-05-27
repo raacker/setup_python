0dfe343373ff0ce102818d10bc1476882fcf556d
raacker <haven.cpp@gmail.com>
2017-05-27 13:49:31 +0900

    add PATH

--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-apt-get install python3
-apt-get install python3-pip
-pip install --upgrade pip
+apt-get install -y python3
+apt-get install -y python3-pip
+pip3 install --upgrade pip
+
 if [ -f /usr/bin/pip ]; then
 	rm -rf /usr/bin/pip
 fi
 sudo ln -s /usr/local/bin/pip /usr/bin/pip
 
-pip install numpy
-pip install pandas
+pip3 install numpy
+pip3 install pandas
+
+echo "alias python='python3'" >> ~/.bashrc
+echo "alias pip='pip3'" >> ~/.bashrc
