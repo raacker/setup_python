c9e3b2a5db1a530156b32ec77581057c6451a46e
raacker <haven.cpp@gmail.com>
2017-06-14 17:46:07 +0900

    add jupyter

--- run.sh
+++ run.sh
@@ -9,8 +9,11 @@ if [ -f /usr/bin/pip ]; then
 fi
 sudo ln -s /usr/local/bin/pip /usr/bin/pip
 
+pip3 install setuptools
 pip3 install numpy
 pip3 install pandas
+pip3 install jupyter
 
 echo "alias python='python3'" >> ~/.bashrc
 echo "alias pip='pip3'" >> ~/.bashrc
+echo "alias notebook='jupyter-notebook'" >> ~/.bashrc
